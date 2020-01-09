1e82c87b7e92db64bd3c89179ecb42d5381ee926
PoGibas <povilasgibas@gmail.com>
2020-01-09 09:46:53 +0100

    added find motif

---
+++ R/findmotif.R
@@ -0,0 +1,19 @@
+findMotif <- function(
+  path_genome = "/scratch/store/annotations/HomoSapiens_hg19/genome/genome.fa",
+  motif = c("CG", "GC", "GCG")
+) {
+  library(data.table)
+  library(foreach)
+  library(magrittr)
+  library(ShortRead)
+  fasta <- readDNAStringSet(path_genome)
+  for (MOTIF in motif) {
+    foreach(i = seq_along(fasta), .combine = rbind) %do% {
+      matchPattern("CG", mask(fasta[[i]], "N"), fixed = FALSE) %>%
+        start(.) %>%
+        data.table(start = .) %>%
+        .[, .(chr = names(fasta)[i], start)]
+    } %>%
+      saveRDS(paste0("motif_", i, ".RDS"), compress = FALSE)
+  }
+}
